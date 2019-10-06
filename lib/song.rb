03b3970ccd86828180c64d2b278de7cfd95fe9d0
Eddy Tyannikov <etyannikov@gmail.com>
2019-10-06 22:17:15 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  @@genre_count = {}
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    
+    @@genres << genre
+    @@artists << artist
+
+  end
+  
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+end
+
+
+
+
+
+
+
