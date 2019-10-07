c8e018f7c19442c657e07dc3aae1597df0c34e59
Eddy Tyannikov <etyannikov@gmail.com>
2019-10-07 03:01:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   @@artists = []
   @@genre_count = {}
   @@artist_count.new
-  attr_accessor :name, :artists, :genre
+  attr_accessor :name, :artist, :genre
 
   def initialize(name, artist, genre)
     @name = name
